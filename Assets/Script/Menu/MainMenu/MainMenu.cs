e53523e0483acba3f4cc0591967ad61e992fdf9d
Gaëtan Peronnet <gaetan.peronnet@gmail.com>
2023-03-13 10:57:45 +0100

    added scene

---
+++ Assets/Script/Menu/MainMenu/MainMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+    public void SettingsMenu()
+    {
+        SceneManager.LoadScene("SettingsMenuScene");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
